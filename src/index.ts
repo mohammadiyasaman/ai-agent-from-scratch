8c4652d0d19e3f9fec9c28fc468fa0246b600b07
yasaman <yas75531@gmail.com>
2026-07-31 21:31:31 +0330

    feat(tools): add filesystem tools (read, write, list)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import { registerTool } from "./tools/registry";
-import { calculatorTool } from "./tools/calculator";
-import { startLoop } from "./agent/start-loop";
 
+import { calculatorTool } from "./tools/calculator";
+
+import { readFileTool } from "./tools/filesystem/read-file";
+
+import { writeFileTool } from "./tools/filesystem/write-file";
+
+import { listFilesTool } from "./tools/filesystem/list-files";
+
+import { startLoop } from "./agent/start-loop";
 
 registerTool(calculatorTool);
 
+registerTool(readFileTool);
+
+registerTool(writeFileTool);
+
+registerTool(listFilesTool);
 
 startLoop();
